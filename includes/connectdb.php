8286e6b37191ffaba58fe87f61d75dba68f5a374
theerapat <66010914105@msu.ac.th>
2026-03-08 16:51:30 +0700

    Update connectdb.php

--- includes/connectdb.php
+++ includes/connectdb.php
@@ -1,8 +1,9 @@
 <?php
         $host = "localhost";
         $user = "root";
-        $pwd = "";
+        $pwd = "915800";
         $db = "guitar_shop_db";
         $conn = mysqli_connect($host,$user,$pwd,$db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
         mysqli_query($conn,"SET NAMES utf8");
+
 ?>
